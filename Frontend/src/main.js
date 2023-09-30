536aaf2e38a09cc521cc2719168838f830d8ddb0
Orbán Gábor <o.gabor007@gmail.com>
2023-09-30 08:45:03 +0200

    addActor

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -33,23 +33,13 @@ window.onload = () => {
     let city = createCity(mapChuncks, scene)
     scene.add(city)
 
-    const loader = new OBJLoader();
-    loader.load('./objects/car1.obj', function (obj) {
-        // Create a mesh from the loaded object
-        // const mesh = new THREE.Mesh(obj.children[0].geometry, new THREE.MeshBasicMaterial({ color: 0xffffff }));
-        // Add the mesh to the scene
-        obj.position.set(0, 0, 0)
-        obj.scale.set(0.02, 0.02, 0.02)
-        obj.rotateY(Math.PI / 2)
-        obj.position.x -= 1.3
-        obj.position.z -= 5
-        obj.position.y += 0.2
-        scene.add(obj);
-    });
+    createMainCar(scene)
+    addActor("kid","kid1", city, 0.5, 0.2, 0.5, 2)
+    addActor("mother","mother1", city)
 
 
     camera.position.y = 3;
-    camera.position.z = -10;
+    camera.position.z = -5;
     camera.position.x = -1;
     camera.rotateY(Math.PI)
     camera.rotateX(-Math.PI / 15)
@@ -57,4 +47,65 @@ window.onload = () => {
 
     animationAllowed = true
     animate(render, scene, camera, city, 0)
+}
+
+
+function createMainCar(scene){
+    const loader = new OBJLoader();
+
+    loader.load('./objects/car1.obj', function (obj) {
+        obj.position.set(0, 0, 0)
+        obj.scale.set(0.02, 0.02, 0.02)
+        obj.rotateY(Math.PI / 2)
+        obj.position.x -= 1.3
+        obj.position.z -= 0
+        obj.position.y += 0.2
+        obj.name = "mainCar"
+        
+        scene.add(obj);
+    });
+}
+
+function addActor(type, name, scene, x = 0, y = 0, z = 0, r = 0){
+    const loader = new OBJLoader();
+
+    if(type === "kid"){
+        loader.load('./objects/kid.obj', function (obj) {
+            obj.position.set(0, 0, 0)
+            obj.scale.set(0.35,0.35,0.35)
+            obj.rotateY(Math.PI / r)
+            obj.position.x += x
+            obj.position.z += z
+            obj.position.y += y
+            obj.name = name
+            
+            scene.add(obj);
+        });
+    }
+    else if(type==="mother"){
+        loader.load('./objects/mother.obj', function (obj) {
+            obj.position.set(0, 0, 0)
+            obj.scale.set(0.01,0.01,0.01)
+            obj.rotateY(Math.PI / r)
+            obj.position.x += x
+            obj.position.z += z
+            obj.position.y += y
+            obj.name = name
+            
+            scene.add(obj);
+        });
+    }
+    else if(type==="car"){
+        loader.load('./objects/car1.obj', function (obj) {
+            obj.position.set(0, 0, 0)
+            obj.scale.set(0.01,0.01,0.01)
+            obj.rotateY(Math.PI / r)
+            obj.position.x += x
+            obj.position.z += z
+            obj.position.y += y
+            obj.name = name
+            
+            scene.add(obj);
+        });
+    }
 }
