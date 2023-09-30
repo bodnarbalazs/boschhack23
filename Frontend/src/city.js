d273decaef247dc14feec766f0f02d7920916597
Orbán Gábor <o.gabor007@gmail.com>
2023-09-30 05:34:36 +0200

    add texture

--- Frontend/src/city.js
+++ Frontend/src/city.js
@@ -6,6 +6,8 @@ export function createCity(mapChuncks, scene){
 
     console.log(mapChuncks)
 
+    let road = new THREE.Group();
+
     for(let chunk of mapChuncks){
         chunk.direction = getDirection(prevChunckDirection, chunk.turn)
         chunk.start = {
@@ -16,15 +18,24 @@ export function createCity(mapChuncks, scene){
 
         for(let i = 0; i < chunk.length; i++){
             if(i == 0 && chunk.turn != null){
-                let result = placeCrossRoad(position, chunk, scene)
+                let result = placeCrossRoad(position, chunk, road)
                 position = result.position
-                scene = result.scene
-                chunk.start.position = position
+                road = result.road
+                chunk.start.position = {
+                    x: position.x,
+                    y: position.y,
+                    z: position.z
+                }
             }
                 
             let geometry = new THREE.BoxGeometry( 6, 0.5, 1 );
-            let material = new THREE.MeshBasicMaterial( {color: "gray"} );
-            let cube = new THREE.Mesh( geometry, material );
+
+            const textureLoader = new THREE.TextureLoader();
+            const roadTexture = textureLoader.load('./textures/texture2.jpg');
+            roadTexture.wrapS = THREE.RepeatWrapping;
+            roadTexture.repeat.set(1, .3); 
+            const roadMaterial = new THREE.MeshBasicMaterial({ map: roadTexture });
+            let cube = new THREE.Mesh( geometry, roadMaterial );
             cube.position.set(position.x, position.y, position.z)
             
 
@@ -43,7 +54,7 @@ export function createCity(mapChuncks, scene){
                 position.z -= 1
             }
             
-            scene.add( cube );
+            road.add( cube );
         }
         prevChunckDirection = chunk.direction
         chunk.end = {
@@ -51,15 +62,14 @@ export function createCity(mapChuncks, scene){
             y: position.y,
             z: position.z
         }
-
-        scene = drawLanes(chunk, scene)
     }
 
-    console.log(position)
+    let city = new THREE.Group()
+    
+    city = placePlain(position, city)
+    city.add(road)
 
-    scene = placePlain(position, scene)
-
-    return scene
+    return city
 }
 
 function getDirection(prevDirection, turn){
@@ -102,35 +112,16 @@ function getDirection(prevDirection, turn){
 }
 
 
-function drawLanes(chunk, scene){
-    console.log(chunk.start, chunk.end)
-    const start = new THREE.Vector3(chunk.start.x, chunk.start.y + 0.28, chunk.start.z);
-    const end = new THREE.Vector3(chunk.end.x, chunk.end.y + 0.28, chunk.end.z);
-  
-    const material = new THREE.LineDashedMaterial({
-      color: 0xffffff,
-      linewidth: 1,
-      scale: 1,
-      dashSize: 3,
-      gapSize: 1,
-    });
-  
-    const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
-    const line = new THREE.Line(geometry, material);
-    line.computeLineDistances();
-  
-    scene.add(line);
-
-    return scene
-}
-
-
-function placeCrossRoad(position, chunk, scene){
-    let geometry = new THREE.BoxGeometry( 6, 0.5, 6 );
-    let material = new THREE.MeshBasicMaterial( {color: "gray"} );
-    let cube = new THREE.Mesh( geometry, material );
+function placeCrossRoad(position, chunk, road){
+    let geometry = new THREE.BoxGeometry( 6, 0.58, 6 );
+    const textureLoader = new THREE.TextureLoader();
+    const roadTexture = textureLoader.load('./textures/texture2_corner.jpg');
+    roadTexture.wrapS = THREE.RepeatWrapping;
+    roadTexture.repeat.set(1, 0.5); 
+    const roadMaterial = new THREE.MeshBasicMaterial({ map: roadTexture });
+    let cube = new THREE.Mesh( geometry, roadMaterial );
     cube.position.set(position.x, position.y, position.z)
-    scene.add( cube );
+    road.add( cube );
 
     if(chunk.direction === "forward"){
         position.z += 3
@@ -145,18 +136,18 @@ function placeCrossRoad(position, chunk, scene){
         position.z -= 3
     }
 
-    return {position, scene}
+    return {position, road}
 }
 
 
-function placePlain(position, scene){
-    let width = 30
-    let depth = 30
+function placePlain(position, city){
+    let width = 1000
+    let depth = 1000
 
     let geometry = new THREE.BoxGeometry( width, 0.5, depth );
     let material = new THREE.MeshBasicMaterial( {color: "wheat"} );
     let cube = new THREE.Mesh( geometry, material );
-    cube.position.set(0, -0.1, depth/2-2)
-    scene.add( cube );
-    return scene
+    cube.position.set(0, -0.1, 0)
+    city.add( cube );
+    return city
 }
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,35 +5,35 @@ import { createCity } from './city.js'
 let animationAllowed = false //this is a flag to stop the animation loop
 
 const mapChuncks = [
-    {turn: null, length: 10},
-    {turn: "right", length: 5},
-    {turn: "left", length: 15},
-    {turn: "left", length: 20},
-    {turn: "right", length: 10}
+    {turn: null, length: 20},
+    {turn: "left", length: 10},
+    {turn: "left", length: 15}
 ]
 
-function animate(render, scene, camera, frameCount = 0) {
+function animate(render, scene, camera, city, frameCount) {
     if(!animationAllowed) 
         return
 
     // console.log(`Frame: ${frameCount}`) //logging the frame count to the console
 
+    city.position.z -= 0.1
     
     render.render(scene, camera)
-    requestAnimationFrame(() => animate(render, scene, camera, ++frameCount))
+    requestAnimationFrame(() => animate(render, scene, camera, city, ++frameCount))
 }
 
 
 window.onload = () => {
     let { scene, camera, render} = createScene()
+    scene.add(new THREE.DirectionalLight(0xffffff, 1))
 
-    scene = createCity(mapChuncks, scene)
+    let city = createCity(mapChuncks, scene)
+    scene.add(city)
 
-    camera.position.y = 50;
+    camera.position.y = 3;
     camera.position.z = 0;
-
-    camera.lookAt(0, 0, 6)
+    camera.rotateX(-Math.PI / 8)
 
     animationAllowed = true
-    animate(render, scene, camera)
+    animate(render, scene, camera, city, 0)
 }
